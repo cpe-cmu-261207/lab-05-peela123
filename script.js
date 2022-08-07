9ad17ad46db93b38081ba56743523edb80aef3fa
Peera Arunrat <peelaxv@gmail.com>
2022-08-07 20:21:38 +0700

    finish

--- script.js
+++ script.js
@@ -5,10 +5,18 @@ inputAdd.onkeyup = (event) => {
   if (event.key !== "Enter") return;
 
   //your code here
+  
+  if(inputAdd.value === "") return  alert("Todo cannot be empty"); 
+  addTodo(inputAdd.value,false);
+  inputAdd.value = ""
+ 
+
 };
 
 function addTodo(title, completed) {
   //create a div that holds todo title, done button, delete button
+  console.log(title);
+  console.log(completed);
   const div = document.createElement("div");
   div.className = "border-bottom p-1 py-2 fs-2 d-flex";
 
@@ -30,19 +38,74 @@ function addTodo(title, completed) {
 
   //your code here
   //append todo to HTML...
+  div.appendChild(span)
+  div.appendChild(doneBtn)
+  div.appendChild(deleteBtn)
+  
+  
+  todoCtn.prepend(div)
   //define buttons event...
+
+  
+  doneBtn.style.display = "none"
+  deleteBtn.style.display = "none"
+  div.onmouseover = () => {
+    doneBtn.style.display = ""
+    deleteBtn.style.display = ""
+  }
+  div.onmouseout = () => {
+    doneBtn.style.display = "none"
+    deleteBtn.style.display = "none"
+  }
+  doneBtn.onclick = () => {
+    if(span.style.textDecoration === ""){
+      span.style.textDecoration === "line-through";
+    }
+    else{
+      span.style.textDecoration = ""
+      
+    }
+
+  }
+  deleteBtn.onclick = () => {
+    todoCtn.removeChild(div);
+  }
+  saveTodo();
+  
+
+
+  
+  
 }
 
 function saveTodo() {
   const data = [];
   for (const todoDiv of todoCtn.children) {
     //your code here
+     const todoObj = {}
+     todoObj.title = todoDiv.children[0].innerText
+     todoObj.completed = todoDiv.children[0].style.textDecoration === "line-through"
+     data.push(todoObj)
+     
+    
   }
+
+  
   //your code here
+  localStorage.setItem("todoList",JSON.stringify(data))
+ 
 }
 
 function loadTodo() {
   //your code here
+  const data = JSON.parse(localStorage.getItem("todoList"))
+  for(const x of data.reverse()){
+    addTodo(x.title,x.completed)
+
+
+  }
+ 
 }
 
 loadTodo();
+
